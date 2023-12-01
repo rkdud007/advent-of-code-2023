0310d3892ef45c214941744bb3b37dec7bec11ca
Pia <gayeongparkk@gmail.com>
2023-12-02 00:44:48 +0900

    This is super dumb but solved #2

--- day1/src/main.rs
+++ day1/src/main.rs
@@ -1,4 +1,4 @@
-use std::fs;
+use std::{collections::HashMap, fs};
 
 use regex::{Captures, Regex};
 
@@ -7,21 +7,9 @@ fn main() {
     println!("Infile {}", file_path);
     let contents = fs::read_to_string(file_path).expect("Something went wrong reading the file");
 
-    // let re = Regex::new(r"\bone\b").unwrap();
-    // let result = re.replace_all(&contents, "1");
-
-    // let mut result = contents.as_str().replace("one", "1");
-    // result = result.as_str().replace("two", "2");
-    // result = result.as_str().replace("three", "3");
-    // result = result.as_str().replace("four", "4");
-    // result = result.as_str().replace("five", "5");
-    // result = result.as_str().replace("six", "6");
-    // result = result.as_str().replace("seven", "7");
-    // result = result.as_str().replace("eight", "8");
-    // result = result.as_str().replace("nine", "9");
-
-    let re = Regex::new("(one|two|three|four|five|six|seven|eight|nine)").unwrap();
-    let result = re.replace_all(contents.as_str(), |cap: &Captures| {
+    let re_front = Regex::new("(one|two|three|four|five|six|seven|eight|nine)").unwrap();
+    let re_back = Regex::new("(eno|owt|eerht|ruof|evif|xis|neves|thgie|enin)").unwrap();
+    let result_front = re_front.replace_all(contents.as_str(), |cap: &Captures| {
         match &cap[0] {
             "one" => "1",
             "two" => "2",
@@ -36,64 +24,61 @@ fn main() {
         }
         .to_string()
     });
-    // println!("{}", result);
-    println!("Contents {}", result);
-    let mut values: Vec<u32> = vec![];
-    let mut temp = 0;
-    let mut first = true;
-    let mut index = 0;
-    // let mut char_check = vec![];
 
-    for c in result.chars() {
-        if c.is_numeric() {
-            let value = c.to_digit(10).unwrap();
-            if first {
-                values.push(value * 10);
-                first = false;
-            }
-            temp = value;
+    let binding = contents.chars().rev().collect::<String>();
+    let result_back = re_back.replace_all(&binding, |cap: &Captures| {
+        match &cap[0] {
+            "eno" => "1",
+            "owt" => "2",
+            "eerht" => "3",
+            "ruof" => "4",
+            "evif" => "5",
+            "xis" => "6",
+            "neves" => "7",
+            "thgie" => "8",
+            "enin" => "9",
+            _ => panic!("We should never get here"),
         }
-        //else {
-        //     char_check.push(c.to_string());
-        //     let is_match = char_check.join("");
-        //     let matched_num = match is_match.as_str() {
-        //         "one" => 1,
-        //         "two" => 2,
-        //         "three" => 3,
-        //         "four" => 4,
-        //         "five" => 5,
-        //         "six" => 6,
-        //         "seven" => 7,
-        //         "eight" => 8,
-        //         "nine" => 9,
-        //         _ => 0,
-        //     };
-        //     if matched_num != 0 {
-        //         char_check = vec![]
-        //     }
+        .to_string()
+    });
+    // println!("{}", result);
+    println!("Contents {}", result_front);
+    println!("Contents {}", result_back);
+    let mut result: HashMap<usize, u32> = HashMap::with_capacity(1000);
 
-        // println!("check:{}, {}", is_match, matched_num);
-
-        if c == '\n' {
-            let mut first_letter = values[index];
-            first_letter += temp;
-            values[index] = first_letter;
-            println!("values: {:?}, index:{}", values, index);
-            temp = 0;
-            index += 1;
-            first = true;
-        };
+    for (front_index, front_line) in result_front.lines().enumerate() {
+        println!("{}, {}", front_line, front_index);
+        for front_char in front_line.chars() {
+            if front_char.is_numeric() {
+                result.insert(front_index, front_char.to_digit(10).unwrap());
+                println!(
+                    "line front_char      {}     {}",
+                    front_char.to_digit(10).unwrap(),
+                    front_index
+                );
+                break;
+            }
+        }
     }
 
-    let mut first_letter = values[index];
-    first_letter += temp;
-    values[index] = first_letter;
-    println!("values: {:?}, index:{}", values, index);
+    let length = result_back.lines().count();
 
-    let mut sum = 0;
-    for value in values {
-        sum += value;
+    println!("{:?}     {:?}", result, length);
+    let mut answer = 0;
+    for (back_index, back_line) in result_back.lines().enumerate() {
+        println!("{}, {}  ", back_line, back_index);
+        let mut line_answer = 0;
+        for back_char in back_line.chars() {
+            if back_char.is_numeric() {
+                let reverse_back_index = length - 1 - back_index;
+                let front_char = *result.get(&reverse_back_index).unwrap();
+                println!("line back    {}     {}", front_char, reverse_back_index);
+                line_answer += front_char * 10 + back_char.to_digit(10).unwrap();
+                println!("line answer{}", line_answer);
+                break;
+            }
+        }
+        answer += line_answer;
     }
-
-    println!("final answer: {}", sum);
+    println!("line answer{}", answer);
 }
